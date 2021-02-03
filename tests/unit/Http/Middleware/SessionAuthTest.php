781dbe577c5150c9618ecbd2acd175667e8ddc47
Derek Stephen McLean <delboy1978uk@gmail.com>
2021-02-03 23:38:59 +0100

    test exception handling

--- tests/unit/Http/Middleware/SessionAuthTest.php
+++ tests/unit/Http/Middleware/SessionAuthTest.php
@@ -12,6 +12,7 @@ use Del\Person\Entity\Person;
 use Del\Person\Service\PersonService;
 use Del\Service\UserService;
 use Del\SessionManager;
+use Exception;
 use Laminas\Diactoros\ServerRequest;
 use ParagonIE\Paseto\JsonToken;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -27,20 +28,23 @@ class SessionAuthTest extends Test
     /** @var MockObject $userService */
     private $userService;
 
+    /** @var MockObject $pasetoService */
+    private $pasetoService;
+
     public function _before()
     {
         $user = new User();
         $person = new Person();
         $user->setPerson($person);
         $token = $this->createMock(JsonToken::class);
-        $pasetoService = $this->createMock(PasetoService::class);
+        $this->pasetoService = $this->createMock(PasetoService::class);
         $personService = $this->createMock(PersonService::class);
         $this->userService = $this->createMock(UserService::class);
-        $this->middleware = new SessionAuth(SessionManager::getInstance(), $this->userService, $pasetoService);
+        $this->middleware = new SessionAuth(SessionManager::getInstance(), $this->userService, $this->pasetoService);
         $this->userService->method('getPersonSvc')->willReturn($personService);
         $this->userService->method('findUserById')->willReturn($user);
         $personService->method('toArray')->willReturn([]);
-        $pasetoService->method('decryptToken')->willReturn($token);
+        $this->pasetoService->method('decryptToken')->willReturn($token);
         $token->method('getClaims')->willReturn(['user' => 1]);
     }
 
@@ -81,6 +85,21 @@ class SessionAuthTest extends Test
     {
         $request = new ServerRequest();
         $request = $request->withCookieParams(['resu' => 1]);
+        $handler = new class implements RequestHandlerInterface {
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                return new Response();
+            }
+        };
+        $response = $this->middleware->process($request, $handler);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+
+    public function testTokenException()
+    {
+        $request = new ServerRequest();
+        $request = $request->withCookieParams(['resu' => 1]);
+        $this->pasetoService->method('decryptToken')->willThrowException(new Exception());
         $handler = new class implements RequestHandlerInterface {
             public function handle(ServerRequestInterface $request): ResponseInterface
             {
