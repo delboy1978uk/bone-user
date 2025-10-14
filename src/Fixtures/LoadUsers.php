2d3be3bfcae069321e20417bdb75514e0da95f6f
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-10-14 17:21:37 +0200

    default user fixtures

--- src/Fixtures/LoadUsers.php
+++ src/Fixtures/LoadUsers.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Bone\User\Fixtures;
 
+use DateTime;
+use DateTimeZone;
 use Del\Entity\User;
 use Del\Person\Entity\Person;
+use Del\Value\User\State;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,12 +16,17 @@ class LoadUsers implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $password = password_hash('123456', PASSWORD_BCRYPT, ['cost' => 14]);
+        $registrationDate = new DateTime('now', new DateTimeZone('UTC'));
         $person = new Person();
         $person->setFirstName('Super');
         $person->setLastName('McUser');
         $user = new  User();
-        $user->getPerson($person);
+        $user->setPerson($person);
         $user->setEmail('man@work.com');
+        $user->setPassword($password);
+        $user->setState(new State(State::STATE_ACTIVATED));
+        $user->setRegistrationDate($registrationDate);
         $manager->persist($user);
         $manager->flush();
 
@@ -26,8 +34,11 @@ class LoadUsers implements FixtureInterface
         $person->setFirstName('Super');
         $person->setLastName('McAdmin');
         $user = new  User();
-        $user->getPerson($person);
+        $user->setPerson($person);
         $user->setEmail('staff@work.com');
+        $user->setPassword($password);
+        $user->setState(new State(State::STATE_ACTIVATED));
+        $user->setRegistrationDate($registrationDate);
         $manager->persist($user);
         $manager->flush();
 
@@ -35,8 +46,11 @@ class LoadUsers implements FixtureInterface
         $person->setFirstName('Norma');
         $person->setLastName('McUser');
         $user = new  User();
-        $user->getPerson($person);
+        $user->setPerson($person);
         $user->setEmail('woman@home.com');
+        $user->setPassword($password);
+        $user->setState(new State(State::STATE_ACTIVATED));
+        $user->setRegistrationDate($registrationDate);
         $manager->persist($user);
         $manager->flush();
     }
